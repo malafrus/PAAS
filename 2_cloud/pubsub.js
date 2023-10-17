217dfcb6b3e772c0c275b71599d9d14980b67062
malafrus <53238766+malafrus@users.noreply.github.com>
2023-10-17 12:42:10 +0200

    Update pubsub.js

--- 2_cloud/pubsub.js
+++ 2_cloud/pubsub.js
@@ -1,41 +1,42 @@
 const { Storage } = require('@google-cloud/storage');
 
 
-const urlApiJCDecaux = "https://api.jcdecaux.com/vls/v1/stations?contract=amiens&apiKey=01bf135f69fc184acbdcdfbf926e670b1358bfb8"
 
-const projectId = ' androidprojecttrochon';
-const bucketName = 'malafrus-bucket-velib-libre'; 
+exports.Trigger = () =>{
 
-//const keyFilename = './key.json'   //local
-//const storage = new Storage({ keyFilename,projectId });
+    const urlApiJCDecaux = "https://api.jcdecaux.com/vls/v1/stations?contract=amiens&apiKey=01bf135f69fc184acbdcdfbf926e670b1358bfb8"
 
-const storage = new Storage({ projectId }); //cloud
+    const projectId = ' androidprojecttrochon';
+    const bucketName = 'malafrus-bucket-velib-libre'; 
 
-async function fetchApiAndSaveInBucket() {
-    try {
-        const response = await fetch(urlApiJCDecaux);
-        if (!response.ok) {
-            throw new Error('Erreur de requête : ' + response.status);
+    //const keyFilename = './key.json'   //local
+    //const storage = new Storage({ keyFilename,projectId });
+
+    const storage = new Storage({ projectId }); //cloud
+
+    async function fetchApiAndSaveInBucket() {
+        try {
+            const response = await fetch(urlApiJCDecaux);
+            if (!response.ok) {
+                throw new Error('Erreur de requête : ' + response.status);
+            }
+
+            const data = await response.json();
+
+            const timestamp = Date.now(); 
+            const fileName = `data_${timestamp}.json`;
+        
+
+            const bucket = storage.bucket(bucketName);
+            const file = bucket.file(fileName);
+
+            await file.save(JSON.stringify(data), {
+                contentType: 'application/json',
+            });
+            console.log('Données stockées avec succès dans le bucket GCS.');
+        } catch (error) {
+            console.error('Une erreur s\'est produite :', error);
         }
-
-        const data = await response.json();
-
-        const timestamp = Date.now(); 
-        const fileName = `data_${timestamp}.json`;
-    
-
-        const bucket = storage.bucket(bucketName);
-        const file = bucket.file(fileName);
-
-        await file.save(JSON.stringify(data), {
-            contentType: 'application/json',
-        });
-        console.log('Données stockées avec succès dans le bucket GCS.');
-    } catch (error) {
-        console.error('Une erreur s\'est produite :', error);
     }
-}
-
-functions.http('Trigger', (req, res) => {
     fetchApiAndSaveInBucket();
-  });
+}
